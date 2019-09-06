0d2ab3fb6bde2361892f7af4c473d299b74f292f
vitor quintanilha barbosa <vitorqb@gmail.com>
2019-09-06 19:43:27 +0200

    NEW Corrects user db, gitignore and tests

--- resources/migrations/20190906191702-add-users-table.up.sql
+++ resources/migrations/20190906191702-add-users-table.up.sql
@@ -1,9 +1,3 @@
 CREATE TABLE users
 (id VARCHAR(20) PRIMARY KEY,
- first_name VARCHAR(30),
- last_name VARCHAR(30),
- email VARCHAR(30),
- admin BOOLEAN,
- last_login TIMESTAMP,
- is_active BOOLEAN,
- pass VARCHAR(300));
+ email VARCHAR(50));
