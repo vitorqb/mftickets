a8f9227c8550b22a1c948ca2dd040ff1b1473987
vitor quintanilha barbosa <vitorqb@gmail.com>
2019-09-14 09:02:37 +0200

    Uses cookie store instead of ttl

--- src/clj/mftickets/middleware.clj
+++ src/clj/mftickets/middleware.clj
@@ -3,13 +3,17 @@
     [mftickets.env :refer [defaults]]
     [mftickets.config :refer [env]]
     [mftickets.middleware.cors :as middleware.cors]
-    [ring-ttl-session.core :refer [ttl-memory-store]]
+    [ring.middleware.session.cookie]
     [ring.middleware.defaults :refer [site-defaults wrap-defaults]]))
 
 (defn wrap-base [handler]
-  (-> ((:middleware defaults) handler)
-      middleware.cors/allow-cors-header
-      (wrap-defaults
-        (-> site-defaults
-            (assoc-in [:security :anti-forgery] false)
-            (assoc-in  [:session :store] (ttl-memory-store (* 60 30)))))))
+
+  (let [{:keys [cookie-store-key]} env
+        cookie-store (ring.middleware.session.cookie/cookie-store {:key cookie-store-key})]
+
+    (-> ((:middleware defaults) handler)
+        middleware.cors/allow-cors-header
+        (wrap-defaults
+         (-> site-defaults
+             (assoc-in [:security :anti-forgery] false)
+             (assoc-in [:session :store] cookie-store))))))
