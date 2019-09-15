e7543ff9d703f7828e680ef8e9c58c313e7af2cc
vitor quintanilha barbosa <vitorqb@gmail.com>
2019-09-15 13:18:00 +0200

    NEW Template migrations and code for example template insertion

---
+++ src/clj/mftickets/db/prefill.clj
@@ -0,0 +1,28 @@
+(ns mftickets.db.prefill
+  "A namespace responsible for pre-filling information on the db."
+  (:require
+   [clojure.java.jdbc :as jdbc]
+   [clojure.core.match :refer [match]]
+   [mftickets.db.prefill.example-template :as db.prefill.example-template]))
+
+(defn- parse-args
+  "Parses a sequence of [:tableName {args}] into a list of jdbc/insert! calls."
+  [[tableName args & nextArgs] {:keys [db]}]
+  {:pre [(not (nil? db))]}
+  (loop [tableName* tableName
+         args* args
+         todo nextArgs
+         done []]
+    (match [tableName* args*]
+      [nil nil] done
+      [nil _]   (throw (ex-info "Invalid arguments: args was nil but tableName not." {}))
+      [_ nil]   (throw (ex-info "Invalid arguments: tableName was nil but args not." {}))
+      [_ _]     (let [result [jdbc/insert! db tableName* args*]
+                      nextDone (conj done result)
+                      [nextTableName nextArgs & nextTodo] todo]
+                  (recur nextTableName nextArgs nextTodo nextDone)))))
+
+(defn prefill-effects
+  "Returns a map of prefill effects, that can be run using db.core/run!"
+  [{:keys [db] :as opts}]
+  {:example-template (parse-args db.prefill.example-template/example-template-prefill opts)})
---
+++ test/clj/mftickets/db/prefill_test.clj
@@ -0,0 +1,30 @@
+(ns mftickets.db.prefill-test
+  (:require [mftickets.db.prefill :as sut]
+            [clojure.test :as t :refer [is are deftest testing use-fixtures]]
+            [clojure.java.jdbc :as jdbc]))
+
+(deftest test-parse-args
+
+  (let [parse-args #'sut/parse-args]
+
+    (testing "Empty"
+      (let [args []
+            opts {:db ::db}
+            result (parse-args args opts)]
+        (is (= [] result))))
+
+    (testing "One Long"
+      (let [args [:tableName {:arg1 :value1 :arg2 :value2}]
+            opts {:db ::db}
+            result (parse-args args opts)]
+        (is (= [[jdbc/insert! ::db :tableName {:arg1 :value1 :arg2 :value2}]]
+               result))))
+
+    (testing "Two Long"
+      (let [args [:table1 {:arg1 :value1}
+                  :table2 {:arg2 :value2}]
+            opts {:db ::db}
+            result (parse-args args opts)]
+        (is (= [[jdbc/insert! ::db :table1 {:arg1 :value1}]
+                [jdbc/insert! ::db :table2 {:arg2 :value2}]]
+               result))))))
