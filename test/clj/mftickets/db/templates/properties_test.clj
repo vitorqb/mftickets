151899e19b7d713f25142957f6f6004cfaf5620e
vitor quintanilha barbosa <vitorqb@gmail.com>
2019-09-20 19:13:51 +0200

    NEW Uses test factories on two tests

--- test/clj/mftickets/db/templates/properties_test.clj
+++ test/clj/mftickets/db/templates/properties_test.clj
@@ -1,26 +1,18 @@
 (ns mftickets.db.templates.properties-test
   (:require [mftickets.db.templates.properties :as sut]
             [clojure.test :as t :refer [is are deftest testing use-fixtures]]
-            [mftickets.test-utils :as test-utils]))
+            [mftickets.test-utils :as tu]))
 
 (deftest test-get-properties-for-template
 
   (testing "Empty"
-    (test-utils/with-db
+    (tu/with-db
       (is (= [] (sut/get-properties-for-template {:id 1})))))
 
   (testing "Base"
-    (test-utils/with-db
-      (test-utils/insert! :templateSections {:id 1 :templateId 9 :name "Foo"})
-      (test-utils/insert! :templateSectionProperties
-                          {:id 1
-                           :templateSectionId 1
-                           :name "Bar"
-                           :isMultiple false
-                           :valueType "section.property.value.types/text"})
-      (is (= [{:id 1
-               :template-section-id 1
-               :name "Bar"
-               :is-multiple false
-               :value-type :section.property.value.types/text}]
-             (sut/get-properties-for-template {:id 9}))))))
+    (tu/with-db
+      (tu/gen-save! tu/template-section {:id 1})
+      (let [template-section-property (tu/gen-save! tu/template-section-property
+                                                    {:template-section-id 1})]
+        (is (= [template-section-property]
+               (sut/get-properties-for-template {:id 9})))))))
